8a5ba7726763afd27206fe6081d54fdf678c536d
Abraham <abrahambogale747@gmail.com>
2026-05-29 17:00:53 +0300

    Network failure game draw return fix 9

--- src/pages/GameLayout.jsx
+++ src/pages/GameLayout.jsx
@@ -1292,7 +1292,7 @@ export default function GameLayout({ stake, onNavigate }) {
                             </td>
                           );
                         })}
-                      </table>
+                      </tr>
                     ))}
                   </tbody>
                 </table>
