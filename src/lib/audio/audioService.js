3b59de592e4db1e53daebc38e288f06184c53cfa
Abraham <abrahambogale747@gmail.com>
2026-06-06 22:48:33 +0300

    Sound change 11

--- src/lib/audio/audioService.js
+++ src/lib/audio/audioService.js
@@ -53,7 +53,7 @@ class AudioService {
     if (!this.audioContext) return null;
 
     const key = `${letter}${number}`;
-    const url = `https://www.epidemicsound.com/sound-effects/tracks/3db7eb51-2bfc-465c-93ad-3e630c6b12d5/`;
+    const url = `/sound/${letter}${number}.mp3`;
 
     if (this.sounds.has(key)) return this.sounds.get(key);
 
@@ -108,7 +108,7 @@ class AudioService {
 
   playHtml5Sound(letter, n) {
     return new Promise((resolve) => {
-      const audioPath = `https://www.epidemicsound.com/sound-effects/tracks/3db7eb51-2bfc-465c-93ad-3e630c6b12d5/`;
+      const audioPath = `/sound/${letter}${n}.mp3`;
       const audio = new Audio(audioPath);
       audio.volume = 0.7;
 
@@ -151,7 +151,7 @@ class AudioService {
 
     for (let n = 1; n <= 75; n++) {
       const letter = this.getLetterForNumber(n);
-      const audio = new Audio(`https://www.epidemicsound.com/sound-effects/tracks/3db7eb51-2bfc-465c-93ad-3e630c6b12d5/`);
+      const audio = new Audio(`/sound/${letter}${n}.mp3`);
       audio.preload = "auto";
       audio.load();
       if (n % 20 === 0) {
