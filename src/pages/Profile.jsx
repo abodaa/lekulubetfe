3fe7c5935b71dc067a82577f53f275530df4e756
Abraham <abrahambogale747@gmail.com>
2026-06-20 12:49:32 +0300

    Language switch added

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import BottomNav from "../components/BottomNav";
 import { useAuth } from "../lib/auth/AuthProvider";
-import { useLanguage } from "../lib/i18n/LanguageContext";
+import { useLanguage } from "../contexts/Languagecontext";
 import { apiFetch } from "../lib/api/client";
 import { motion } from "framer-motion";
 import {
